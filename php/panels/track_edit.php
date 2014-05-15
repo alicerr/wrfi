ba7f3524baa1133662a19cd63e520c64d978fc30
Alice Renegar <arr233@cornell.edu>
2014-05-14 21:07:35 -0400

    JACKIE PANEL UPDATE

--- php/panels/track_edit.php
+++ php/panels/track_edit.php
@@ -1,70 +1,38 @@
 <!--add or edit a track-->
 	<form method="post">
-		<input type="number" name="track_id" class="hide"
-		       value = "<?php echo(get_post("track_id")); ?>" />
+		<input type="number" name="track_id" class="hide" value = "<?php echo(get_post("track_id")); ?>" />
 
-	<!--title and time info-->	
-        Track title: <input type="text" name="track_name" id="sid_track_name"
-	value = "<?php echo(get_post("track_name")); ?>" required /><br />
-	
-	Starting time:
-	<input type="number" min = "0" max = "12" width = "2" name="start_hour"
-	       	value = "<?php echo(get_post("start_hour")); ?>" required />:
-	<input type="number" min = "0" max = "59" width = "2" name="start_min"
-	       	value = "<?php echo(get_post("start_min")); ?>" required />:
-        <input type="number" min = "0" max = "59" width = "2" name="start_sec"
-	       	value = "<?php echo(get_post("start_sec")); ?>" required />
-	<select name = "start_am_pm" value = "<?php echo(get_post("start_am_pm")); ?>" >
-		<option value ="am">AM</option>
-		<option value ="pm">PM</option>
-	</select>
-	
-	<input type="date" name="start_date"
-	       	value = "<?php echo(get_post("start_date")); ?>" required />
-	<br />
-	Playtime:
-	<input type="number" min = "0" max = "64" width = "2" name="duration_min"
-	       	value = "<?php echo(get_post("duration_min")); ?>" required />:
-	<input type="number" min = "0" max = "59" width = "2" name="duration_sec" required 
-		       	value = "<?php echo(get_post("duration_sec")); ?>" />
-	
-	Full Track Length:
-	<input type="number" min = "0" max = "12" width = "2" name="length_hour"
-	        	value = "<?php echo(get_post("length_hour")); ?>" />:
-	<input type="number" min = "0" max = "12" width = "2" name="length_min"
-	        	value = "<?php echo(get_post("length_min")); ?>" />:
-	<input type="number" min = "0" max = "59" width = "2" name="length_sec"
-	        	value = "<?php echo(get_post("length_sec")); ?>" /><br />
-
-	<input number = "text" class = "hide" name = "artist_id"
-	       value = "<?php echo(get_post("artist_id")); ?>" />
-	<!--artist info-->
-        Artist name: <input type="text" name="artist_name"
-	value = "<?php echo(get_post("artist_name")); ?>" />
-	        Artist website:  https://www.<input type="text" name="artist_website"
-	value ="<?php echo(get_post("artist_website")); ?>" /> <br />
-        Artist description: <input type = "text" name="artist_desc"
-		value = "<?php echo(get_post("artist_desc")); ?>" />
-
-
-	<br>
-		
-        <!--album info-->
-	<input type="number" name="album_id" class="hide"
-		       value = "<?php echo(get_post("album_id")); ?>" />
-	Album: <input type="text" name="album_id" 
-		       value = "<?php echo(get_post("album_name")); ?>" />
-	Album title: <input type="text" name="album_name"
-	value = "<?php echo(get_post("album_name")); ?>" /> <br />
-	Album Website: https://www.<input type="number" name="album_name" 
-		       value = "<?php echo(get_post("album_website")); ?>" /> <br />
-				      
-	<!--label info-->
-
-	Label: <input type="text" name="label_name"
-	 	value = "<?php echo(get_post("label_name")); ?>" />
-		
-	Label website: https://www.<input type="text" name="label_website"
-	 	value = "<?php echo(get_post("label_website")); ?>" />
-        <input type="submit" value="Save changes" name="update_track" />
+		<!--title and time info-->	
+        <label>Track title:</label><input type="text" name="track_name" id="sid_track_name" value = "<?php echo(get_post("track_name")); ?>" required /><br>
+		<label>Starting time:</label><input type="number" min = "0" max = "12" width = "2" name="start_hour" value = "<?php echo(get_post("start_hour")); ?>" required />:
+		<input type="number" min = "0" max = "59" width = "2" name="start_min" value = "<?php echo(get_post("start_min")); ?>" required />:
+	    <input type="number" min = "0" max = "59" width = "2" name="start_sec" value = "<?php echo(get_post("start_sec")); ?>" required />
+		<select name = "start_am_pm" value = "<?php echo(get_post("start_am_pm")); ?>" >
+			<option value ="am">AM</option>
+			<option value ="pm">PM</option>
+		</select>
+		<input type="date" name="start_date" value = "<?php echo(get_post("start_date")); ?>" required />
+		<br>
+		<label>Playtime:</label>
+		<input type="number" min = "0" max = "64" width = "2" name="duration_min" value = "<?php echo(get_post("duration_min")); ?>" required />:
+		<input type="number" min = "0" max = "59" width = "2" name="duration_sec" required value = "<?php echo(get_post("duration_sec")); ?>" /><br>
+		<label>Full Track Length:</label>
+		<input type="number" min = "0" max = "12" width = "2" name="length_hour" value = "<?php echo(get_post("length_hour")); ?>" />:
+		<input type="number" min = "0" max = "12" width = "2" name="length_min" value = "<?php echo(get_post("length_min")); ?>" />:
+		<input type="number" min = "0" max = "59" width = "2" name="length_sec" value = "<?php echo(get_post("length_sec")); ?>" /><br>
+		<input number = "text" class = "hide" name = "artist_id"value = "<?php echo(get_post("artist_id")); ?>" />
+		<!--artist info-->
+	    <label>Artist name:</label><input type="text" name="artist_name" value = "<?php echo(get_post("artist_name")); ?>" /><br>
+		<label>Artist website:</label>https://www.<input type="text" name="artist_website" value ="<?php echo(get_post("artist_website")); ?>" /> <br>
+	    <label>Artist description:</label><input type = "text" name="artist_desc" value = "<?php echo(get_post("artist_desc")); ?>" /><br>
+		<br/>
+	    <!--album info-->
+		<input type="number" name="album_id" class="hide" value = "<?php echo(get_post("album_id")); ?>" />
+		<label>Album:</label><input type="text" name="album_id" value = "<?php echo(get_post("album_name")); ?>" /><br>
+		<label>Album title:</label><input type="text" name="album_name" value = "<?php echo(get_post("album_name")); ?>" /> <br>
+		<label>Album Website:</label>https://www.<input type="text" name="album_website" value = "<?php echo(get_post("album_website")); ?>" /><br>	      
+		<!--label info-->
+		<label>Label:</label><input type="text" name="label_name" value = "<?php echo(get_post("label_name")); ?>" /><br>
+		<label>Label website:</label>https://www.<input type="text" name="label_website" value = "<?php echo(get_post("label_website")); ?>" /><br>
+	    <input type="submit" value="Save changes" name="update_track" />
     </form>
--- php/panels/user_edit.php
+++ php/panels/user_edit.php
@@ -1,12 +1,12 @@
 
 <form method="post">
-    	First name: <input type="text" name="fname" required />
-    	Last name: <input type="text" name="lname" />
-    	Phone #: <input type="text" name="phone" />
-    	<input type="text" name="user_id" class="hide" value = "<?php get_post("user_id"); ?>"/>
-	Email: <input type="email" name="email" />
-	Password:<input type = "password" name = "password" required />
-	New Password (leave blank to keep current password):<input type = "password" name = "new_password" />
-    	Confirm New Password: <input type = "password" name = "confirm_password" />
+    <label>First name:</label><input type="text" name="fname" required /><br>
+	<label>Last name:</label><input type="text" name="lname" /><br>
+	<label>Phone #:</label><input type="text" name="phone" /><br>
+    <input type="text" name="user_id" class="hide" value = "<?php get_post("user_id"); ?>"/>
+	<label>Email:</label><input type="email" name="email" /><br>
+	<label>Password:</label><input type = "password" name = "password" required /><br>
+	<label>New Password (leave blank to keep current password):</label><input type = "password" name = "new_password" /><br>
+    <label>Confirm New Password:</label><input type = "password" name = "confirm_password" /><br>
 	<input type="submit" value="Update My Settings" name = "update_user" />
-    </form>
+</form>
